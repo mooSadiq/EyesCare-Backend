3d19f3defec08d9360a94c0b428a74cb9342d466
Malek ALmosanif <Almosanif1095@gmail.com>
2024-10-01 02:52:39 +0300

    Update Eye Disease Detect Functions

--- Dashboard/apps/diagnosis/Eye_Diseases_Detect.py
+++ Dashboard/apps/diagnosis/Eye_Diseases_Detect.py
@@ -43,23 +43,23 @@ def resize_image(image_path):
 
 def disease_detect(image_path):
     image=resize_image(image_path)
-    result = Detect_Eye.classify_and_save_image(image)
+    result = classify_and_save_image(image)
     if result == "Eye":
-        image,label=run_model(image)
-        return image,label
+        image,label,conf=run_model(image)
+        return image,label,conf
     
     elif result=="Internal-Eye":
-        image,label=classify_internalEye_image(image)
-        return image,label
+        image,label,conf=classify_internalEye_image(image)
+        return image,label,conf
     
     elif result == "No eye detected":
-        return None, "No eye detected in the image."
+        return None, "No eye detected in the image.",None
     
     elif result == "No sufficient confidence.":
-        return None, "The model did not find a confident prediction."
+        return None, "The model did not find a confident prediction.",None
     
     else:
-        return None, "Error during RobowFlow inference hint:'Check Internet'."
+        return None, "Error during RobowFlow inference hint:'Check Internet'.",None
 
 def run_model(image):
     response = CLIENT.infer(image, "ccatract/4")
@@ -67,42 +67,29 @@ def run_model(image):
     predictions = response.get('predictions', [])
     
     if predictions:
-        label = None  # تعيين قيمة افتراضية للـ label
         for prediction in predictions:
-            label = prediction.get('class')
-            # image, label = draw_box(image, prediction)
-        return image, label
+            image, label,conf = draw_box(image, prediction)
+        return image, label,conf
     else:
-        return image, "No Diseases detected."
-        # response = CLIENT.infer(image, "ccatract/4")
-        # if response['predictions']:
-        #     for prediction in response['predictions']:
-        #         image, label=draw_box(image,prediction)
-        #     return image, label
-        # else:
-        #     # return None, "No Diseases detected."
-        #     return image, "No Diseases detected."
+        return image, "No Diseases detected.",None
 
 def draw_box(image,prediction):
                 x, y = prediction['x'], prediction['y']
                 width, height = prediction['width'], prediction['height']
                 predicted_conf=prediction['confidence']*100
                 label = prediction['class']
-                # label = prediction.get('class')
                 start_point = (int(x - width / 2), int(y - height / 2))
                 end_point = (int(x + width / 2), int(y + height / 2))
                 text_position = (start_point[0], start_point[1] - 10)
-                # Draw the bounding box and label on the image
                 formatted_conf = f"{predicted_conf:.1f}"
                 text = f"{label} {formatted_conf}"
                 cv2.rectangle(image, start_point, end_point, (255, 0, 0), 2)
                 cv2.putText(image, text, text_position, cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
-                return image,label
+                return image,label,predicted_conf
 
 def classify_internalEye_image(image):
     classification_results = image_classifier(image)
     best_prediction = max(classification_results, key=lambda x: x["score"])
     confidence_score = "{:f}".format(best_prediction["score"])
     predicted_label = best_prediction["label"]
-    label_confidence=f'{predicted_label} _ {confidence_score}'
-    return None,label_confidence
+    return None,predicted_label,confidence_score
